259a503663e20f76cda226f1caa1c5256008a7dc
Aditya kumar <viratbadriwaldancer@gmail.com>
2026-02-07 18:35:25 +0530

    first commit

--- backend/food_server.js
+++ backend/food_server.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+
 mongoose.connect('mongodb://127.0.0.1:27017/foodAppp')
   .then(() => console.log("✅ Connected to Database"))
   .catch(err => console.error("❌ Connection error:", err));
