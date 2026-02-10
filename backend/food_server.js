4aa4af6898938a3ed691b58cb1672a85a748b1cf
Aditya kumar <viratbadriwaldancer@gmail.com>
2026-02-10 11:18:39 +0530

    tiger bro

--- backend/food_server.js
+++ backend/food_server.js
@@ -66,5 +66,4 @@ app.delete('/api/food/:id', async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server: http://localhost:${PORT}`));
-// food_server.js ke aakhir mein
-module.exports = app;
+
